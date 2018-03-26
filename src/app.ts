e217a0324a1445c87687acc614178a4bf3a2b6a9
Chris Ip <nydragon@gmail.com>
2018-03-25 17:53:09 -0700

    Add a cCircle class and 2 instances of it

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,48 @@
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
 
+interface iShape {
+  draw(): void;
+  x: number;
+  y: number;
+  color: string;
+  lineWidth: number;
+}
+
+class cCircle implements iShape {
+  public x: number = 0;
+  public y: number = 0;
+  public radius: number = 10;
+  public lineWidth: number = 2;
+  public color: string = 'red';
+  constructor(x: number, y: number, radius: number, color: string = 'red', lineWidth: number = 2) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.lineWidth = lineWidth;
+  }
+
+  public draw = (): void => {
+    ctx.save();
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.lineWidth;
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.restore();
+  }
+}
+
+const circle1: cCircle = new cCircle(200, 300, 50);
+const circle2: cCircle = new cCircle(400, 550, 150, 'blue', 5);
+
 const gameLoop = () => {
   requestAnimationFrame(gameLoop);
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, 1280, 720);
-  ctx.beginPath();
-  ctx.strokeStyle = 'red';
-  ctx.lineWidth = 5;
-  ctx.arc(400, 400, 100, 0, 2 * Math.PI);
-  ctx.stroke();
+  circle1.draw();
+  circle2.draw();
 }
 
 window.onload = () => {

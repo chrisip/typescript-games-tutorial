034c815bb69b8f4bd28ff7115f3bbc01c4dc510f
Chris Ip <nydragon@gmail.com>
2018-03-25 23:18:49 -0700

    Add Point and Asteroid classes to shape.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
+import * as Canvas from './canvas';
 import * as Shape from './shape';
 
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
-
-const CANVAS_WIDTH = 1280;
-const CANVAS_HEIGHT = 720;
 
 const circle1: Shape.Circle = new Shape.Circle(200, 300, 50);
 const circle2: Shape.Circle = new Shape.Circle(400, 550, 150, 'blue', 5);
@@ -12,11 +10,11 @@ const circle2: Shape.Circle = new Shape.Circle(400, 550, 150, 'blue', 5);
 const gameLoop = () => {
   requestAnimationFrame(gameLoop);
   ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  if (circle1.x++ >= (CANVAS_WIDTH + circle1.radius)) {
+  ctx.fillRect(0, 0, Canvas.WIDTH, Canvas.HEIGHT);
+  if (circle1.x++ >= (Canvas.WIDTH + circle1.radius)) {
     circle1.x = -circle1.radius;
   }
-  if (circle2.y++ >= (CANVAS_HEIGHT + circle2.radius)) {
+  if (circle2.y++ >= (Canvas.HEIGHT + circle2.radius)) {
     circle2.y = -circle2.radius;
   }
   circle1.draw(ctx);
--- src/shape.ts
+++ src/shape.ts
@@ -1,3 +1,5 @@
+import * as Canvas from './canvas';
+
 export interface Shape {
   draw(ctx: CanvasRenderingContext2D): void;
   x: number;
@@ -42,6 +44,108 @@ export class Rectangle implements Shape {
     ctx.strokeStyle = this.color;
     ctx.lineWidth = this.lineWidth;
     ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+    ctx.restore();
+  }
+}
+
+export class Point {
+  constructor(
+      public x: number = 0,
+      public y: number = 0,
+  ) {}
+}
+
+export class Asteroid implements Shape {
+  public velocityX: number = 0;
+  public velocityY: number = 0;
+  public rotation: number = 0;
+  public rotationSpeed: number = 0;
+  public pointList: Array<Point> = new Array<Point>();
+
+  constructor(
+      public x: number = undefined,
+      public y: number = undefined,
+      public size?: number,
+      public color: string = 'white',
+      public lineWidth: number =2,
+  ) {
+    if (!x) {
+      this.x = Math.round(Math.random() * Canvas.WIDTH);
+    }
+    if (!y) {
+      this.y = Math.round(Math.random() * Canvas.HEIGHT);
+    }
+    if (!size) {
+      this.size = Math.ceil(Math.random() * 10) + 4;
+    }
+    this.velocityX = Math.round(Math.random() * 4 - 2);
+    this.velocityY = Math.round(Math.random() * 4 - 2);
+    this.rotationSpeed = Math.random() * 0.06 - 0.03;
+    let xRand: number = 0;
+    let yRand: number = 0;
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand, yRand + 3 * this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand - 1 * this.size, yRand + 2 * this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand - 2 * this.size, yRand + 2 * this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand - 3 * this.size, yRand + this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand - 4 * this.size, yRand));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand - 1 * this.size, yRand - 3 * this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand + 2 * this.size, yRand - 4 * this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand + 2 * this.size, yRand - 3 * this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand + 4 * this.size, yRand - 2 * this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand + 4 * this.size, yRand + this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+    this.pointList.push(new Point(xRand + 3 * this.size, yRand + 2 * this.size));
+    xRand = Math.round(Math.random() * this.size - this.size / 2);
+    yRand = Math.round(Math.random() * this.size - this.size / 2);
+  }
+
+  public draw = (ctx: CanvasRenderingContext2D): void => {
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+    if (this.x < -this.size * 2) {
+      this.x = Canvas.WIDTH + this.size * 2;
+    } else if (this.x > Canvas.WIDTH + this.size * 2) {
+      this.x = -2 * this.size;
+    }
+    if (this.y < -this.size * 2) {
+      this.y = Canvas.HEIGHT + this.size * 2;
+    } else if (this.y > Canvas.HEIGHT + this.size * 2) {
+      this.y = -2 * this.size;
+    }
+    this.rotation += this.rotationSpeed;
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.rotation);
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.lineWidth;
+    ctx.moveTo(this.pointList[this.pointList.length - 1].x, this.pointList[this.pointList.length - 1].y);
+    for (let i: number = 0; i < this.pointList.length; i++) {
+      ctx.lineTo(this.pointList[i].x, this.pointList[i].y);
+    }
+    ctx.closePath();
     ctx.stroke();
     ctx.restore();
   }

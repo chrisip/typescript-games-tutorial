634b9284042bcbe25d20559c2c7b5ba16ecdd82f
Chris Ip <nydragon@gmail.com>
2018-03-25 17:42:22 -0700

    Draw a circle

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ var gameLoop = function () {
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, 1280, 720);
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 5;
+    ctx.arc(400, 400, 100, 0, 2 * Math.PI);
+    ctx.stroke();
 };
 window.onload = function () {
     canvas = document.getElementById('cnvs');
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,11 @@ const gameLoop = () => {
   requestAnimationFrame(gameLoop);
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, 1280, 720);
+  ctx.beginPath();
+  ctx.strokeStyle = 'red';
+  ctx.lineWidth = 5;
+  ctx.arc(400, 400, 100, 0, 2 * Math.PI);
+  ctx.stroke();
 }
 
 window.onload = () => {

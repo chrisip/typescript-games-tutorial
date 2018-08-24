6ed2f908774b7a87cd77350840fb51510794c6fd
Chris Ip <nydragon@gmail.com>
2018-08-24 00:23:44 -0700

    Add inputLoop to Keyboard class

--- src/input.ts
+++ src/input.ts
@@ -1,8 +1,11 @@
 type Callback = () => void;
 
+type KeyCode = number;
+
 export class Keyboard {
-  public keyCallback: { [keyCode: number]: Callback } = {};
-  public keyDown: { [keyCode: number]: boolean } = {};
+  public keyCallback: Map<KeyCode, Callback> = new Map<KeyCode, Callback>();
+  public keyDown: Map<KeyCode, boolean> = new Map<KeyCode, boolean>();
+
 
   constructor() {
     document.addEventListener('keydown', this.keyboardDown);
@@ -11,15 +14,26 @@ export class Keyboard {
 
   public keyboardDown(event: KeyboardEvent): void {
     event.preventDefault();
-    this.keyDown[event.keyCode] = true;
+    this.keyDown.set(event.keyCode, true);
   }
 
   public keyboardUp(event: KeyboardEvent): void {
-    this.keyDown[event.keyCode] = false;
+    this.keyDown.set(event.keyCode, false);
   }
 
   public addKeyCodeCallback(keyCode: number, f: Callback): void {
-    this.keyCallback[keyCode] = f;
-    this.keyDown[keyCode] = false;
+    this.keyCallback.set(keyCode, f);
+    this.keyDown.set(keyCode, false);
+  }
+
+  public inputLoop(): void {
+    this.keyDown.forEach((isDown, keyCode) => {
+      if (isDown) {
+        const callback = this.keyCallback.get(keyCode);
+        if (callback) {
+          callback();
+        }
+      }
+    });
   }
 }

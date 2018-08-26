a50676d757116c32ac548c7d9784920bcbd4a0d1
Chris Ip <nydragon@gmail.com>
2018-08-25 23:43:03 -0700

    Add Keyboard input loop to game loop; Rearrange app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,35 +6,14 @@ import * as Stat from './stat';
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
 
-const gameLoop = () => {
-  requestAnimationFrame(gameLoop);
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, Canvas.WIDTH, Canvas.HEIGHT);
-  shapeList.forEach(shape => {
-    shape.draw(ctx);
-  });
-  spaceship.bulletList.forEach(bullet => {
-    bullet.draw(ctx);
-  })
-  reporter.draw();
-}
-
-const shapeList: Array<Shape.Shape> = new Array<Shape.Shape>();
-
-const asteroid: Shape.Asteroid = new Shape.Asteroid();
-asteroid.velocityX = 0;
-asteroid.velocityY = 0;
-
-const spaceship: Shape.Spaceship = new Shape.Spaceship(200, 450, 5);
-
 let reporter: Stat.Reporter;
-let keyboardInput: Input.Keyboard;
+const shapeList: Array<Shape.Shape> = new Array<Shape.Shape>();
+const spaceship: Shape.Spaceship = new Shape.Spaceship(200, 450, 5);
+const keyboardInput: Input.Keyboard = new Input.Keyboard();;
 
 window.onload = () => {
   canvas = <HTMLCanvasElement>document.getElementById('cnvs');
-  document.addEventListener('keydown', keyboardInput);
   shapeList.push(spaceship);
-  keyboardInput = new Input.Keyboard();
   keyboardInput.addKeyCodeCallback(Input.Key.Left, spaceship.turnLeft);
   keyboardInput.addKeyCodeCallback(Input.Key.A, spaceship.turnLeft);
   keyboardInput.addKeyCodeCallback(Input.Key.Up, spaceship.accelerate);
@@ -47,4 +26,18 @@ window.onload = () => {
   reporter =  new Stat.Reporter('stats', spaceship);
   ctx = canvas.getContext('2d');
   gameLoop();
+}
+
+const gameLoop = () => {
+  requestAnimationFrame(gameLoop);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, Canvas.WIDTH, Canvas.HEIGHT);
+  keyboardInput.inputLoop();
+  shapeList.forEach(shape => {
+    shape.draw(ctx);
+  });
+  spaceship.bulletList.forEach(bullet => {
+    bullet.draw(ctx);
+  })
+  reporter.draw();
 }

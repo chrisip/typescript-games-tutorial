6fdf96bbe4bb2ce41395154b6fd1048e508c6a5a
Chris Ip <nydragon@gmail.com>
2018-08-26 18:53:09 -0700

    Add testing of circle collision

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as Canvas from './canvas';
+import * as Collision from './collision';
 import * as Input from './input';
 import * as Shape from './shape';
 import * as Stat from './stat';
@@ -10,12 +11,18 @@ class Game {
   private shapeList: Array<Shape.Shape>;
   private spaceship: Shape.Spaceship;
   private reporter: Stat.Reporter;
+  // TODO: Remove this after finishing testing collisions
+  private circle1: Collision.CircleCollider;
+  private circle2: Collision.CircleCollider;
 
   constructor() {
     this.keyboardInput = new Input.Keyboard();
     this.shapeList = new Array<Shape.Shape>();
     this.spaceship = new Shape.Spaceship(200, 450, 5);
     this.reporter = new Stat.Reporter('stats', this.spaceship);
+    // TODO: Remove this after finishing testing collisions
+    this.circle1 = new Collision.CircleCollider();
+    this.circle2 = new Collision.CircleCollider();
     this.keyboardInput.addKeyCodeCallback(Input.Key.Left, this.spaceship.turnLeft);
     this.keyboardInput.addKeyCodeCallback(Input.Key.A, this.spaceship.turnLeft);
     this.keyboardInput.addKeyCodeCallback(Input.Key.Up, this.spaceship.accelerate);
@@ -24,7 +31,9 @@ class Game {
     this.keyboardInput.addKeyCodeCallback(Input.Key.D, this.spaceship.turnRight);
     this.keyboardInput.addKeyCodeCallback(Input.Key.Down, this.spaceship.decelerate);
     this.keyboardInput.addKeyCodeCallback(Input.Key.S, this.spaceship.decelerate);
-    this.keyboardInput.addKeyCodeCallback(Input.Key.Space, this.spaceship.shoot);
+    // TODO: Remove this after finishing testing collisions
+    // this.keyboardInput.addKeyCodeCallback(Input.Key.Space, this.spaceship.shoot);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.Space, this.resetCircles);
     this.shapeList.push(this.spaceship);
     this.shapeList.push(new Shape.Asteroid());
     this.shapeList.push(new Shape.Asteroid());
@@ -48,8 +57,41 @@ class Game {
     this.spaceship.bulletList.forEach(bullet => {
       bullet.draw(this.ctx);
     })
+    // TODO: Remove this after finishing testing collisions
+    this.drawCircles();
     this.reporter.draw();
   }
+
+    // TODO: Remove this after finishing testing collisions
+    public drawCircle = (x: number, y: number, radius: number, color: string): void => {
+      this.ctx.save();
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = color;
+      this.ctx.lineWidth = 5;
+      this.ctx.arc(x, y, radius, 0, 2 * Math.PI);
+      this.ctx.stroke();
+      this.ctx.restore();
+    }
+
+    // TODO: Remove this after finishing testing collisions
+    public drawCircles = (): void => {
+      let color = 'blue';
+      if (Collision.Collision.CircleCircle(this.circle1, this.circle2)) {
+        color = 'red';
+      }
+      this.drawCircle(this.circle1.position.x, this.circle1.position.y, this.circle1.radius, color);
+      this.drawCircle(this.circle2.position.x, this.circle2.position.y, this.circle2.radius, color);
+    }
+  
+    // TODO: Remove this after finishing testing collisions
+    public resetCircles = (): void => {
+      this.circle1.radius = Math.floor(Math.random() * 400);
+      this.circle1.position.x = this.circle1.radius / 2 + Math.floor(Math.random() * (Canvas.WIDTH - this.circle1.radius / 2));
+      this.circle1.position.y = this.circle1.radius / 2 + Math.floor(Math.random() * (Canvas.HEIGHT - this.circle1.radius / 2));
+      this.circle2.radius = Math.floor(Math.random() * 400);
+      this.circle2.position.x = this.circle2.radius / 2 + Math.floor(Math.random() * (Canvas.WIDTH - this.circle2.radius / 2));
+      this.circle2.position.y = this.circle2.radius / 2 + Math.floor(Math.random() * (Canvas.HEIGHT - this.circle2.radius / 2));
+    }
 }
 
 const game = new Game();

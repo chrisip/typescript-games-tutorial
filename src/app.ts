ae6d250f43261dfa3536eea844b6f7b478e7fead
Chris Ip <nydragon@gmail.com>
2018-08-26 18:32:14 -0700

    Move onload and gameLoop into a Game class

--- src/app.ts
+++ src/app.ts
@@ -3,44 +3,55 @@ import * as Input from './input';
 import * as Shape from './shape';
 import * as Stat from './stat';
 
-let canvas: HTMLCanvasElement;
-let ctx: CanvasRenderingContext2D;
+class Game {
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+  private keyboardInput: Input.Keyboard;
+  private shapeList: Array<Shape.Shape>;
+  private spaceship: Shape.Spaceship;
+  private reporter: Stat.Reporter;
 
-let reporter: Stat.Reporter;
-const keyboardInput: Input.Keyboard = new Input.Keyboard();;
-const shapeList: Array<Shape.Shape> = new Array<Shape.Shape>();
-const spaceship: Shape.Spaceship = new Shape.Spaceship(200, 450, 5);
+  constructor() {
+    this.keyboardInput = new Input.Keyboard();
+    this.shapeList = new Array<Shape.Shape>();
+    this.spaceship = new Shape.Spaceship(200, 450, 5);
+    this.reporter = new Stat.Reporter('stats', this.spaceship);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.Left, this.spaceship.turnLeft);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.A, this.spaceship.turnLeft);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.Up, this.spaceship.accelerate);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.W, this.spaceship.accelerate);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.Right, this.spaceship.turnRight);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.D, this.spaceship.turnRight);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.Down, this.spaceship.decelerate);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.S, this.spaceship.decelerate);
+    this.keyboardInput.addKeyCodeCallback(Input.Key.Space, this.spaceship.shoot);
+    this.shapeList.push(this.spaceship);
+    this.shapeList.push(new Shape.Asteroid());
+    this.shapeList.push(new Shape.Asteroid());
+    this.shapeList.push(new Shape.Asteroid());
+  }
 
-window.onload = () => {
-  canvas = <HTMLCanvasElement>document.getElementById('cnvs');
-  shapeList.push(spaceship);
-  shapeList.push(new Shape.Asteroid());
-  shapeList.push(new Shape.Asteroid());
-  shapeList.push(new Shape.Asteroid());
-  keyboardInput.addKeyCodeCallback(Input.Key.Left, spaceship.turnLeft);
-  keyboardInput.addKeyCodeCallback(Input.Key.A, spaceship.turnLeft);
-  keyboardInput.addKeyCodeCallback(Input.Key.Up, spaceship.accelerate);
-  keyboardInput.addKeyCodeCallback(Input.Key.W, spaceship.accelerate);
-  keyboardInput.addKeyCodeCallback(Input.Key.Right, spaceship.turnRight);
-  keyboardInput.addKeyCodeCallback(Input.Key.D, spaceship.turnRight);
-  keyboardInput.addKeyCodeCallback(Input.Key.Down, spaceship.decelerate);
-  keyboardInput.addKeyCodeCallback(Input.Key.S, spaceship.decelerate);
-  keyboardInput.addKeyCodeCallback(Input.Key.Space, spaceship.shoot);
-  reporter =  new Stat.Reporter('stats', spaceship);
-  ctx = canvas.getContext('2d');
-  gameLoop();
+  public onLoad = () => {
+    this.canvas = <HTMLCanvasElement>document.getElementById('cnvs');
+    this.ctx = this.canvas.getContext('2d');
+    this.gameLoop();
+  }
+
+  public gameLoop = () => {
+    requestAnimationFrame(this.gameLoop);
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(0, 0, Canvas.WIDTH, Canvas.HEIGHT);
+    this.keyboardInput.inputLoop();
+    this.shapeList.forEach(shape => {
+      shape.draw(this.ctx);
+    });
+    this.spaceship.bulletList.forEach(bullet => {
+      bullet.draw(this.ctx);
+    })
+    this.reporter.draw();
+  }
 }
 
-const gameLoop = () => {
-  requestAnimationFrame(gameLoop);
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, Canvas.WIDTH, Canvas.HEIGHT);
-  keyboardInput.inputLoop();
-  shapeList.forEach(shape => {
-    shape.draw(ctx);
-  });
-  spaceship.bulletList.forEach(bullet => {
-    bullet.draw(ctx);
-  })
-  reporter.draw();
-}
+const game = new Game();
+
+window.onload = game.onLoad;
